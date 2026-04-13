561d688c8860ff029b13603fb990f9f925deffaf
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2026-04-14 00:20:58 +0300

    feat(Routes): add Routing & NavBar

--- frontend/eshop-ui/src/App.jsx
+++ frontend/eshop-ui/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import ProductFormPage from "./pages/ProductFormPage";
+import CartPage from "./pages/CartPage";
+import OrderHistoryPage from "./pages/OrderHistoryPage";
+import CategoriesPage from "./pages/CategoriesPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <div className="ticks">
-
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="/products/new" element={<ProductFormPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/products/:id/edit" element={<ProductFormPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/orders" element={<OrderHistoryPage />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </div>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
